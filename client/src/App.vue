7614a54e00e00f99edba9b655ea56e41bd53d714
mengjieliu372 <2792248241@qq.com>
2023-12-04 18:01:55 +0800

    Upload

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <MapData/>
+    <hr>
+    <ShowData/>    
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+import ShowData from './components/ShowData.vue';
+import MapData from './components/MapData.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MapData,
+    ShowData
+}
+}
+</script>
+
+<style>
+</style>
