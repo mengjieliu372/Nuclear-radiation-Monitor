14bffe2fac0b151393f8fc934ae795ffd653f387
mengjieliu372 <2792248241@qq.com>
2023-12-05 17:06:35 +0800

    Upload

---
+++ README.md
@@ -0,0 +1,5 @@
+厦门大学 2023年秋季学期 计算机网络课程项目 / Xiamen University, Fall 2023, Computer Network Final Project
+
+
+
+开发中...暂未完成...
---
+++ my-app/src/components/ShowData.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>Test The Data Returned</p>
+        <div v-for="(item,index) in this.data" v-bind:key="index">
+            <p>{{ item.station }}</p>
+            <p>{{ item.value }}</p>
+            <p>{{ item.time }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ShowData',
+    data() {
+        return {
+            data: {}
+        }
+    },
+    created() {
+        axios.get('http://localhost:3000/returndata-test')
+            .then(response => {
+                //console.log(response.data);
+                this.data = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
